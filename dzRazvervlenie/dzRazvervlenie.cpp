c9609b4fb924f799ebe09587027804ef1fedf773
Иван <112714664+TheVanissimo@users.noreply.github.com>
2022-09-09 23:27:02 +0300

    Add files via upload

---
+++ dzRazvervlenie/dzRazvervlenie.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>;
+#include <cmath>;
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	double x, a, w;
+	cout << "Введите x" << endl;
+	cin >> x;
+	cout << "Введите a" << endl;
+	cin >> a;
+	if (double(abs(x)) < 1) {
+		if (x != 0) {
+			w = a * log(abs(x));
+			cout << "Результат равен: " << w << endl;
+		}
+		else {
+			cout << "Ошибка. Подлогарифмическое выражение равно 0. Решений нет" << endl;
+		}
+	}
+	if (double(abs(x)) >= 1) {
+		if ((a - x * x) >= 0) {
+			w = sqrt(a - x * x);
+			cout << "Результат равен: " << w << endl;
+		}
+		else {
+			cout << "Ошибка. Подкоренное выражение меньше 0. Решений нет" << endl;
+		}
+	}
+}
