9138f988afc96d81e7d843076369f8e6514106f5
Иван <112714664+TheVanissimo@users.noreply.github.com>
2022-09-09 23:30:00 +0300

    Add files via upload

---
+++ Poryadok/Poryadok.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>;
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	int A;
+	cout << "Введите точку отсчета" << endl;
+	cin >> A;
+	for (int counter = A; counter < A+10; counter ++)
+		cout << " " <<counter;
+	cout << endl;
+}
