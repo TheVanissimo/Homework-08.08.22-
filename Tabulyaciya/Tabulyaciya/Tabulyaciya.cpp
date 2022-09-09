a1a5d0e746da2e0a8c864324aae03fa444e30f40
Иван <112714664+TheVanissimo@users.noreply.github.com>
2022-09-09 23:31:00 +0300

    Add files via upload

---
+++ Tabulyaciya/Tabulyaciya/Tabulyaciya.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>;
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	double x, y;
+	for (float x = -4; x <= 4; x += 0.5)
+		if (x != 1) {
+			y = (x * x - 2 * x + 2);
+			cout << "y=" << y << " x=" << x << endl;
+		}
+		else {
+			cout << "Ошибка. Знаменатель равен нулю. Решений нет " << "x=" << x << endl;
+		}
+}
