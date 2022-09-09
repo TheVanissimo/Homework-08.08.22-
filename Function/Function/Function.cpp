47531d0a775b8fa63d6becdb9830855062ea4aa8
Иван <112714664+TheVanissimo@users.noreply.github.com>
2022-09-09 23:28:29 +0300

    Add files via upload

---
+++ Function/Function/Function.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>;
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	double x, y, b, z;
+	cout << "Введите данные" << endl;
+	cout << "Введите x" << endl;
+	cin >> x;
+	cout << "Введите y" << endl;
+	cin >> y;
+	cout << "Введите b" << endl;
+	cin >> b;
+	if ((b - y) > 0) {
+		if ((b - x) >= 0) {
+			z = (log(b - y)) * (sqrt(b - x));
+			cout << "Функция равна: " << z << endl;
+		}
+		else {
+			cout << "Подкоренное выражение меньше 0. Решений нет." << endl;
+		}
+	}
+	else {
+		cout << "Подлогарифмическое выражение меньше/равно 0. Решений нет." << endl;
+	}
+}
